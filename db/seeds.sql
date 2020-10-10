c8987dea36885c1f283205f97fc4140761430fe8
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-10 01:47:38 -0400

    trying to fix heroku authentication issues

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
-INSERT INTO 
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ("State of the Onion Burger", false),
+    ("Lettuce Ketchup Later Burger", false),
+    ("Beef Wellington with Cheese Between Two Slices of Bread", true)
